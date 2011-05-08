407d2b938b5fb2778f0947a9a83de38e146ac3c9
Richard Bolkey <rbolkey@gmail.com>
2011-05-08 00:12:40 -0500

    Added the ability to load an external CoffeeScript compiler.

--- src/coffee_script/core.clj
+++ src/coffee_script/core.clj
@@ -4,36 +4,49 @@
   (:use [clojure.contrib.json :only  (json-str)])
   (:import (org.mozilla.javascript Context ContextAction ContextFactory JavaScriptException Scriptable)))
 
+(defn- run-context-action
+  ([^ContextAction action] (. (ContextFactory/getGlobal) (call action))))
+
+(defn evaluate-compiler
+  [source]
+  (with-open [script (io/reader source)]
+    (run-context-action
+     (reify ContextAction
+       (run [this context]
+         (do (.setOptimizationLevel context -1))
+         (let [^Scriptable x (.initStandardObjects context)]
+           (.evaluateReader context x script "coffee-script.js" 0 nil)
+           x))))))
+
 (def ^{:doc "Default options for compiling coffee script"}
   *default-coffee-options*
   {:bare false})
 
-(defn- run-context-action
-  ([^ContextAction action] (. (ContextFactory/getGlobal) (call action))))
+(declare ^{:doc "The CoffeeScript compiler used for compiling."}
+  *coffee-compiler*)
 
-(def ^{:private true}
-  global-scope
-  (let [x (delay (with-open [script (io/reader (io/resource "coffee_script/coffee-script.js"))]
-                   (run-context-action
-                    (reify ContextAction
-                      (run [this context]
-                        (do (.setOptimizationLevel context -1))
-                        (let [^Scriptable x (.initStandardObjects context)]
-                          (.evaluateReader context x script "coffee-script.js" 0 nil)
-                          x))))))]
-    #(force x)))
+(def builtin-coffee-compiler
+  (evaluate-compiler (io/resource "coffee_script/coffee-script.js")))
+
+(defmacro with-coffee-compiler
+  "Evaluates the body in the context of the coffee compiler coerced from the given input."
+  [c & body]
+  `(binding [*coffee-compiler* (evaluate-compiler ~c)]
+     ~@body))
 
 (defn compile-coffee
-  "Returns a "
+  "Compiles a string of CoffeeScript into its JavaScript form."
   ([^CharSequence source opts]
-     (run-context-action
-      (reify ContextAction
-        (run [this context]
-          (let [^Scriptable scope (doto (. context (newObject (global-scope))) (.setParentScope (global-scope)))]
-            (do (. scope (put "coffeeScriptSource" scope source)))
-            (. context (evaluateString scope
-                                       (format "CoffeeScript.compile(coffeeScriptSource, %s);"
-                                               (json-str opts))
-                                       "lein-coffee-script" 0 nil)))))))
+     (let [compiler (if (bound? (var *coffee-compiler*)) *coffee-compiler* builtin-coffee-compiler)]
+       (run-context-action
+        (reify ContextAction
+          (run [this context]
+            (let [^Scriptable scope (doto (. context (newObject compiler))
+                                      (.setParentScope compiler))]
+              (do (. scope (put "coffeeScriptSource" scope source)))
+              (. context (evaluateString scope
+                                         (format "CoffeeScript.compile(coffeeScriptSource, %s);"
+                                                 (json-str (merge *default-coffee-options* opts)))
+                                         "lein-coffee-script" 0 nil))))))))
   ([^CharSequence source]
      (compile-coffee source {})))
--- test/coffee_script/test_core.clj
+++ test/coffee_script/test_core.clj
@@ -1,8 +1,19 @@
 (ns coffee-script.test-core
-  ""
-  (:use [clojure.test :only [deftest is]]
-	[coffee-script.core :only [compile-coffee]]))
+  "Tests for the core package."
+  (:require [clojure.java.io :as io])
+  (:use [clojure.test :only [deftest is testing]]
+	[coffee-script.core :only [compile-coffee with-coffee-compiler]]))
 
-(deftest test-compile-coffee
-  (is (= "var number;\nnumber = 42;" (compile-coffee "number = 42" {:bare true}))))
-  
+(deftest test-compiler-coffee
+  (testing "Builtin CoffeeScript Compiler"
+    (is (= "var number;\nnumber = 42;"
+           (compile-coffee "number = 42" {:bare true})))
+    (is (= "(function() {\n  var x;\n  x = [1, 2, 3];\n}).call(this);\n"
+           (compile-coffee "x = [1,2,3]"))))
+  (testing "External CoffeeScript Compiler"
+    (is (= "(function() {\n  var number;\n  number = 42;\n}).call(this);\n"
+         (with-coffee-compiler (io/resource "coffee_script/coffee-script.js")
+           (compile-coffee "number = 42"))))
+    (is (= "var x;\nx = [1, 2, 3];"
+         (with-coffee-compiler (io/resource "coffee_script/coffee-script.js")
+           (compile-coffee "x = [1,2,3]" {:bare true}))))))
