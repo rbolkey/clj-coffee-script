2d24b42cb1d26f384f5e0cd72e61d20b84aeedf4
Richard Bolkey <rbolkey@gmail.com>
2011-05-07 21:59:04 -0500

    Moved compile action into a reified ContextAction as well.

--- src/coffee_script/core.clj
+++ src/coffee_script/core.clj
@@ -7,32 +7,32 @@
 (def ^{:doc "Default options for compiling coffee script"} *default-coffee-options*
      {:bare false})
 
+(defn- run-context-action
+  ([action] (. (ContextFactory/getGlobal) (call action))))
+
 (def global-scope
   (let [x (delay (with-open [script (io/reader (io/resource "coffee_script/coffee-script.js"))]
-                   (. (ContextFactory/getGlobal)
-                      (call (reify ContextAction
-                              (run [this context]
-                                (do (.setOptimizationLevel context -1))
-                                (let [x (.initStandardObjects context)]
-                                  (.evaluateReader context x script "coffee-script.js" 0 nil)
-                                  x)))))))]
+                   (run-context-action
+                    (reify ContextAction
+                      (run [this context]
+                        (do (.setOptimizationLevel context -1))
+                        (let [x (.initStandardObjects context)]
+                          (.evaluateReader context x script "coffee-script.js" 0 nil)
+                          x))))))]
     #(force x)))
 
 (defn compile-coffee
   ""
-  ([context scope source opts]
-     (do (. scope (put "coffeeScriptSource" scope source)))
-     (. context (evaluateString scope
-                                (format "CoffeeScript.compile(coffeeScriptSource, %s);"
-                                        (json-str opts))
-                                "lein-coffee-script" 0 nil)))
-  ([context source opts]
-     (compile-coffee context
-                     (doto (. context (newObject (global-scope))) (.setParentScope (global-scope)))
-                     source opts))
   ([source opts]
-     (try (compile-coffee (Context/enter) source opts)
-          (finally (Context/exit))))
+     (run-context-action
+      (reify ContextAction
+        (run [this context]
+          (let [scope (doto (. context (newObject (global-scope))) (.setParentScope (global-scope)))]
+            (do (. scope (put "coffeeScriptSource" scope source)))
+            (. context (evaluateString scope
+                                       (format "CoffeeScript.compile(coffeeScriptSource, %s);"
+                                               (json-str opts))
+                                       "lein-coffee-script" 0 nil)))))))
   ([source]
      (compile-coffee source {})))
 
