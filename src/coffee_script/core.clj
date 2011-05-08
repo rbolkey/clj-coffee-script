891c434eb214259645bd51c10a3847b1eb32beea
Richard Bolkey <rbolkey@gmail.com>
2011-05-07 22:16:38 -0500

    Adding type hints for the rhino api.

--- src/coffee_script/core.clj
+++ src/coffee_script/core.clj
@@ -4,38 +4,36 @@
   (:use [clojure.contrib.json :only  (json-str)])
   (:import (org.mozilla.javascript Context ContextAction ContextFactory JavaScriptException Scriptable)))
 
-(def ^{:doc "Default options for compiling coffee script"} *default-coffee-options*
-     {:bare false})
+(def ^{:doc "Default options for compiling coffee script"}
+  *default-coffee-options*
+  {:bare false})
 
 (defn- run-context-action
-  ([action] (. (ContextFactory/getGlobal) (call action))))
+  ([^ContextAction action] (. (ContextFactory/getGlobal) (call action))))
 
-(def global-scope
+(def ^{:private true}
+  global-scope
   (let [x (delay (with-open [script (io/reader (io/resource "coffee_script/coffee-script.js"))]
                    (run-context-action
                     (reify ContextAction
                       (run [this context]
                         (do (.setOptimizationLevel context -1))
-                        (let [x (.initStandardObjects context)]
+                        (let [^Scriptable x (.initStandardObjects context)]
                           (.evaluateReader context x script "coffee-script.js" 0 nil)
                           x))))))]
     #(force x)))
 
 (defn compile-coffee
-  ""
-  ([source opts]
+  "Returns a "
+  ([^CharSequence source opts]
      (run-context-action
       (reify ContextAction
         (run [this context]
-          (let [scope (doto (. context (newObject (global-scope))) (.setParentScope (global-scope)))]
+          (let [^Scriptable scope (doto (. context (newObject (global-scope))) (.setParentScope (global-scope)))]
             (do (. scope (put "coffeeScriptSource" scope source)))
             (. context (evaluateString scope
                                        (format "CoffeeScript.compile(coffeeScriptSource, %s);"
                                                (json-str opts))
                                        "lein-coffee-script" 0 nil)))))))
-  ([source]
+  ([^CharSequence source]
      (compile-coffee source {})))
-
-(defn coffee [project]
-  "Compile coffee files"
-  )
