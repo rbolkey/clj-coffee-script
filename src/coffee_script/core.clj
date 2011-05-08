451d13df0ee0c07251cea958d96219d6771c6ec0
Richard Bolkey <rbolkey@gmail.com>
2011-05-07 20:47:14 -0500

    Made the global scope lazily instantiatiated.

--- src/coffee_script/core.clj
+++ src/coffee_script/core.clj
@@ -8,12 +8,13 @@
      {:bare false})
 
 (def global-scope
-     (with-open [script (io/reader (io/resource "coffee_script/coffee-script.js"))]
-       (let [context (doto (Context/enter) (.setOptimizationLevel -1))
-	     global (.initStandardObjects context)]
-	 (try (.evaluateReader context global script "coffee-script.js" 0 nil)
-	      (finally (Context/exit)))
-	 global)))
+  (let [x (delay (with-open [script (io/reader (io/resource "coffee_script/coffee-script.js"))]
+           (let [context (doto (Context/enter) (.setOptimizationLevel -1))
+                 global (.initStandardObjects context)]
+             (try (.evaluateReader context global script "coffee-script.js" 0 nil)
+                  (finally (Context/exit)))
+             global)))]
+    #(force x)))
 
 (defn compile-coffee
   ""
@@ -25,7 +26,7 @@
                       "lein-coffee-script" 0 nil))
   ([context source opts]
      (compile-coffee context
-                     (doto (.newObject context global-scope) (.setParentScope global-scope))
+                     (doto (.newObject context (global-scope)) (.setParentScope (global-scope)))
                      source opts))
   ([source opts]
      (try (compile-coffee (Context/enter) source opts)
