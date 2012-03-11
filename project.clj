7872f5a4b5ae67c0988671b4ca8f3e955c9f187c
Richard Bolkey <rbolkey@gmail.com>
2012-03-10 18:49:06 -0600

    Preparing 1.0.1 release
    Patch release to include clojure-contrib as a dependency (for json).

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-coffee-script "1.0.0"
+(defproject clj-coffee-script "1.0.1"
   :description "Clojure wrapper for the CoffeeScript compiler."
   :url "http://www.github.com/rbolkey/clj-coffee-script"
   :license {:name "Eclipse Public License - v 1.0"
@@ -6,6 +6,7 @@
             :distribution :repo
             :comments "same as Clojure"}
   :dependencies [[org.clojure/clojure "1.2.1"]
+                 [org.clojure/clojure-contrib "1.2.0"]
                  [rhino/js "1.7R2"]]
   :dev-dependencies [[lein-difftest "1.3.1"]
                      [swank-clojure "1.3.0"]]
