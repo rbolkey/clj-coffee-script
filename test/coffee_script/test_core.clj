0b97e2007ae821e6963f736dcbaf67112846b97f
Richard Bolkey <rbolkey@gmail.com>
2012-07-22 18:22:36 -0500

    Updating embedded CoffeeScript to v1.3.3

--- test/coffee_script/test_core.clj
+++ test/coffee_script/test_core.clj
@@ -6,14 +6,14 @@
 
 (deftest test-compiler-coffee
   (testing "Builtin CoffeeScript Compiler"
-    (is (= "var number;\nnumber = 42;"
+    (is (= "var number;\n\nnumber = 42;\n"
            (compile-coffee "number = 42" {:bare true})))
-    (is (= "(function() {\n  var x;\n  x = [1, 2, 3];\n}).call(this);\n"
+    (is (= "(function() {\n  var x;\n\n  x = [1, 2, 3];\n\n}).call(this);\n"
            (compile-coffee "x = [1,2,3]"))))
   (testing "External CoffeeScript Compiler"
-    (is (= "(function() {\n  var number;\n  number = 42;\n}).call(this);\n"
+    (is (= "(function() {\n  var number;\n\n  number = 42;\n\n}).call(this);\n"
          (with-coffee-compiler (io/resource "coffee_script/coffee-script.js")
            (compile-coffee "number = 42"))))
-    (is (= "var x;\nx = [1, 2, 3];"
+    (is (= "var x;\n\nx = [1, 2, 3];\n"
          (with-coffee-compiler (io/resource "coffee_script/coffee-script.js")
            (compile-coffee "x = [1,2,3]" {:bare true}))))))
